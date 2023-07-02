83bbc92046130265dcf6bf72571c3a943897e7bc
jstaerk <jstaerk@usegroup.de>
2023-07-02 20:29:44 +0200

    rewriting for one traveler

--- src/main/java/Itinerary.java
+++ src/main/java/Itinerary.java
@@ -11,87 +11,41 @@ import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 public class Itinerary {
 
     private long Id;
-    private int totalTravelTime = -1;
-    @PlanningEntityCollectionProperty
-    private ArrayList <Connection> journey;
-    Timetable t;
+    @PlanningEntityProperty
+    Traveler TravelerA;
+
     private HardSoftScore score;
 
     protected int getStart() {
         return 1;
     }
 
-    protected int getDestination() {
-        return 3;
-    }
-
     public Itinerary() {
-
-        t=new Timetable();
-
-        journey=new ArrayList<Connection>();
+        TravelerA = new Traveler(getStart());
 
     }
+
+    public void createResourcesToOptimize() {
+        TravelerA.createResourcesToOptimize();
+    }
+
 
     public boolean check() {
-        int currentPlace = getStart();
-        int currentTime = 0;
-        int i=0;
-        System.out.println("Deb checking possible journey");
-        for (Connection legU : journey
-        ) {
-        //Connection legU=journey;
-            i++;
-            System.out.println("Deb chk "+i+".."+journey);
+        return TravelerA.check();
+    }
 
 
-            if (legU.startPlaceId != currentPlace) {
-                return false;
-            }
-            if (getDestination() == currentPlace) {
-                return true;
-            }
-            if (legU.from < currentTime) {
-                return false;
-            }
-            currentTime = legU.from + legU.duration;
-            currentPlace = legU.destinationPlaceId;
-        }
-
-        totalTravelTime = currentTime;
-        return currentPlace == getDestination();
+    public int getTotalTravelTime() {
+        return TravelerA.getTotalTravelTime();
     }
 
     public void print() {
-        System.out.println("Score:"+getScore().getHardScore()+"H "+getScore().getSoftScore()+" \nRoute::");
-       // for (Connection leg : journey
+        System.out.println("Score:" + getScore().getHardScore() + "H " + getScore().getSoftScore() + " \nRoute::");
+        // for (Connection leg : journey
         //) {
 
-            System.out.println("R:"+journey);
+        System.out.println("R:" + TravelerA.getJourneyDescription());
         //}
-    }
-
-    public int getTotalTravelTime() {
-        if (totalTravelTime == -1) {
-            check();
-        }
-        return totalTravelTime;
-    }
-
-
-    @ValueRangeProvider(id = "stationRange")
-    public List<Connection> getNextPossibleCollection() {
-        int start=getStart();
-        int time=0;
-        if (journey.size()>0) {
-
-
-            Connection lastConn=journey.get(journey.size());
-            start=lastConn.destinationPlaceId;
-            time=journey.get(journey.size()).from+journey.get(journey.size()).duration;
-        }
-
-        return t.getFutureConnectionsFrom(start, time);
     }
 
     @PlanningScore
@@ -111,23 +65,4 @@ public class Itinerary {
         Id = id;
     }
 
-    public void createResourcesToOptimize() {
-     //journey=new ArrayList<UsedConnection>();
-      //  journey=new Connection();
-        int start=getStart();
-        int time=0;
-        if (journey.size()>0) {
-
-
-            Connection lastConn=journey.get(journey.size());
-            start=lastConn.destinationPlaceId;
-            time=journey.get(journey.size()).from+journey.get(journey.size()).duration;
-        }
-
-        List<Connection> possible=t.getFutureConnectionsFrom(start, time);
-        Random rand = new Random();
-        Connection randomElement = (Connection) possible.get(rand.nextInt(possible.size()));
-        journey.add(randomElement);
-
-    }
 }
--- src/main/java/Traveler.java
+++ src/main/java/Traveler.java
@@ -1,8 +1,11 @@
 import org.optaplanner.core.api.domain.entity.PlanningEntity;
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
 import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
 import org.optaplanner.core.api.domain.variable.PlanningVariable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @PlanningEntity
 public class Traveler {
@@ -11,6 +14,111 @@ public class Traveler {
     int time;
     @PlanningVariable(valueRangeProviderRefs = "nextPoss")
     Connection c;
+
+    Timetable t;
+    private int totalTravelTime = -1;
+
+    protected int getDestination() {
+        return 3;
+    }
+
+
+    @PlanningEntityCollectionProperty
+    private ArrayList<Connection> journey;
+
+
+    Traveler(int startPosId) {
+        journey=new ArrayList<Connection>();
+        t=new Timetable();
+        fromPlaceId=startPosId;
+
+    }
+
+    public boolean check() {
+        int currentPlace = fromPlaceId;
+        int currentTime = 0;
+        int i=0;
+        System.out.println("Deb checking possible journey");
+        for (Connection legU : journey
+        ) {
+            //Connection legU=journey;
+            i++;
+            System.out.println("Deb chk "+i+".."+journey);
+
+
+            if (legU.startPlaceId != currentPlace) {
+                return false;
+            }
+            if (getDestination() == currentPlace) {
+                return true;
+            }
+            if (legU.from < currentTime) {
+                return false;
+            }
+            currentTime = legU.from + legU.duration;
+            currentPlace = legU.destinationPlaceId;
+        }
+
+        totalTravelTime = currentTime;
+        return currentPlace == getDestination();
+    }
+
+    public int getTotalTravelTime() {
+        if (totalTravelTime == -1) {
+            check();
+        }
+        return totalTravelTime;
+    }
+
+    public Traveler() {
+
+    }
+
+    public String getJourneyDescription() {
+        String res="";
+        for (Connection currentConnection: journey
+             ) {
+
+            res+=currentConnection.toString()+"\n";
+        }
+        return res;
+    }
+
+
+    @ValueRangeProvider(id = "stationRange")
+    public List<Connection> getNextPossibleCollection() {
+        int start=fromPlaceId;
+        int time=0;
+        if (journey.size()>0) {
+
+
+            Connection lastConn=journey.get(journey.size());
+            start=lastConn.destinationPlaceId;
+            time=journey.get(journey.size()).from+journey.get(journey.size()).duration;
+        }
+
+        return t.getFutureConnectionsFrom(start, time);
+    }
+
+    public void createResourcesToOptimize() {
+        //journey=new ArrayList<UsedConnection>();
+        //  journey=new Connection();
+        int start=fromPlaceId;
+        int time=0;
+        if (journey.size()>0) {
+
+
+            Connection lastConn=journey.get(journey.size());
+            start=lastConn.destinationPlaceId;
+            time=journey.get(journey.size()).from+journey.get(journey.size()).duration;
+        }
+
+        List<Connection> possible=t.getFutureConnectionsFrom(time, start);
+        Random rand = new Random();
+        Connection randomElement = (Connection) possible.get(rand.nextInt(possible.size()));
+        journey.add(randomElement);
+
+    }
 
     @ValueRangeProvider(id="nextPoss")
     public List<Connection> possibleNext(){
